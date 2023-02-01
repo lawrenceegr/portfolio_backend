903b29d78530a8dc7b12efa1f2bda3c1932e368d
lawrenceegr <lawrencelangatmi@gmail.com>
2023-02-01 13:16:41 +0300

    implemented multer middleware

--- routes/Work.routes.js
+++ routes/Work.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const workController = require('../controllers/Work.controller');
+const upload = require("../middlewares/Upload");
 
 router.get('/', workController.index);
-router.post('/', workController.create);
+router.post('/',upload.single("image"), workController.create);
 router.get('/:id', workController.show);
 router.put('/:id', workController.update);
 router.delete('/:id', workController.delete);
